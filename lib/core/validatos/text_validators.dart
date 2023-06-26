01e6d1735263aaed407ac1dcc12417dc0e4ca09e
Jeison Solarte <jeison.solarte@pragma.com.co>
2023-06-26 15:07:48 -0500

    change web validator

--- lib/core/validatos/text_validators.dart
+++ lib/core/validatos/text_validators.dart
@@ -1,13 +1,13 @@
 class TextValidators {
-  static String? addressWebValidator(String? value, {bool makeStrong = false}) {
+  static String? addressWebValidator(String? value) {
     if (value == null || value.isEmpty) {
       return 'Ingresa tu dirección web';
     }
     if (_validNumber(value.substring(0, 1))) {
       return 'No puede empezar con números';
     }
-    if (makeStrong && !_validAddressWeb(value.replaceAll(RegExp(r'\s+\b|\b\s'), ''))) {
-      return 'Sin caractéres especiales';
+    if (!_validAddressWeb(value)) {
+      return 'No es una url válida';
     }
     return null;
   }
@@ -19,8 +19,8 @@ class TextValidators {
   }
 
   static bool _validAddressWeb(String value) {
-    const String pattern = r'^[A-Za-z0-9_]+$';
-    final RegExp regex = RegExp(pattern);
-    return regex.hasMatch(value);
+    final RegExp urlRegex = RegExp(
+        r'^(?:http|https):\/\/[\w\-_]+(?:\.[\w\-_]+)+[\w\-.,@?^=%&:/~+#]*[\w\-@?^=%&/~+#]$');
+    return urlRegex.hasMatch(value);
   }
 }
